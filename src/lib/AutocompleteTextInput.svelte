f76ed64ee798b652fffd2ff8fe0b0c16155c13fe
Peter York <peter.york@dft.gov.uk>
2024-07-30 15:53:44 +0100

    Remove unecessary exports

--- src/lib/AutocompleteTextInput.svelte
+++ src/lib/AutocompleteTextInput.svelte
@@ -1,30 +1,25 @@
-<script>
+<script lang="ts">
   import { FormElement } from "./index.js";
   import { v4 as uuidv4 } from "uuid";
 
-  export let label;
-  export let name;
-  export let value;
-  export let options;
-  export let dataTestId = "";
+  export let label: string;
+  export let value: string;
+  // A list of [value, label] pairs that can be autocompleted
+  export let options: [string, string][];
 
-  let id = `${name}-${uuidv4()}`;
+  let id = uuidv4();
 </script>
   
-<FormElement {label} id={`${name}-${id}-input`}>
+<FormElement {label} {id} >
   <input
     class="govuk-input govuk-input--width-20"
-    id={`${name}-${id}-input`}
-    data-testid={dataTestId}
-    list={`${name}-${id}-list`}
+    {id}
+    list={`${id}-list`}
     bind:value
   />
-  <datalist id={`${name}-${id}-list`} >
-    {#each options as option}
-      <option 
-        value={option[0]}
-        label = {option[1]}
-      /> 
+  <datalist id={`${id}-list`} >
+    {#each options as [value, label]}
+      <option {value} {label} /> 
     {/each}
   </datalist>
 </FormElement>
