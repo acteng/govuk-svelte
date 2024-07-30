e6ec534cd7b82136cd2dadd77ba307d44b6d2379
Peter York <peter.york@dft.gov.uk>
2024-07-30 14:13:14 +0100

    Make sure the ids are the same across the board

--- src/lib/AutocompleteTextInput.svelte
+++ src/lib/AutocompleteTextInput.svelte
@@ -1,32 +1,30 @@
-<script lang="ts">
-    import { FormElement } from "./index.js";
-    import {onMount} from "svelte";
-    import { v4 as uuidv4 } from "uuid";
-  
-    export let label: string;
-    export let name: string;
-    export let value: string;
-    export let options: [string,string][];
-    export let dataTestId: string = "";
-    
-    let id = `${name}-${uuidv4()}`;
+<script>
+  import { FormElement } from "./index.js";
+  import { v4 as uuidv4 } from "uuid";
+
+  export let label;
+  export let name;
+  export let value;
+  export let options;
+  export let dataTestId = "";
+
+  let id = `${name}-${uuidv4()}`;
 </script>
   
-  <FormElement {label} id={`${name}-${id}-input`}>
-    <input
-      class="govuk-input govuk-input--width-20"
-      id={`${id}-input`}
-      data-testid={dataTestId}
-      list={`${id}-list`}
-      bind:value
-    />
-    <datalist id={`${id}-list`} >
-      {#each options as option}
-        <option 
-          value={option[0]}
-          label = {option[1]}
-        /> 
-      {/each}
-      </datalist>
-  </FormElement>
-  
+<FormElement {label} id={`${name}-${id}-input`}>
+  <input
+    class="govuk-input govuk-input--width-20"
+    id={`${name}-${id}-input`}
+    data-testid={dataTestId}
+    list={`${name}-${id}-list`}
+    bind:value
+  />
+  <datalist id={`${name}-${id}-list`} >
+    {#each options as option}
+      <option 
+        value={option[0]}
+        label = {option[1]}
+      /> 
+    {/each}
+  </datalist>
+</FormElement>
