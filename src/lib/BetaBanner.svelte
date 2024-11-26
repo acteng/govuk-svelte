ac241e384832ff9fe8493f43ecdce5bb20bc9df9
Peter York <peter.york@dft.gov.uk>
2024-11-26 12:22:39 +0000

    Fix typo

--- src/lib/BetaBanner.svelte
+++ src/lib/BetaBanner.svelte
@@ -1,7 +1,7 @@
 <div class="govuk-phase-banner">
   <p class="govuk-phase-banner__content" style="margin-bottom: 0px">
     <strong class="govuk-tag govuk-phase-banner__content__tag">Beta</strong>
-    <span class="govuk-phase-banner__text">n
+    <span class="govuk-phase-banner__text">
       This is a new tool. Contact <a href="mailto:plan-your-active-travel-schemes@activetravelengland.gov.uk">
         the team 
       </a>
